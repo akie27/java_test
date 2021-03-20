5173ccadaa5bee7213a02ab4521671fd49025a9a
akie27 <akie27@outlook.jp>
2021-03-20 11:12:01 +0900

    task2_1.javaを作成しました

---
+++ task2_1.java
@@ -0,0 +1,12 @@
+public class task2_1{
+        public static void main(String[] args){
+        for(int i  = 0; i <= 100; i++){
+            if(i % 2 == 0){
+                System.out.println(i + "は偶数");
+            } else {
+                System.out.println(i + "は奇数");
+            }
+        }
+    }
+}
+
