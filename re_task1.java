0461703e9b8a31b4c183e5291c0df49984dec579
akie27 <akie27@outlook.jp>
2021-03-23 21:15:09 +0900

    re_task1.javaを作成しました

---
+++ re_task1.java
@@ -0,0 +1,13 @@
+public class re_task1 {
+    public static void main(String[] args){
+        for(int i = 7; i < 8; i++) {
+            for(int j = 1; j < 10; j++) {
+            int k = i * j;
+            System.out.print(i + "×" + j + "=" + k);
+            System.out.println("");
+
+        }
+            System.out.println("");
+        }
+    }
+}
