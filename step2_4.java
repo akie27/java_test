e69dc060e0d604238e0cf436604db1bedf9820c5
akie27 <akie27@outlook.jp>
2021-03-27 23:12:14 +0900

    step2_4.javaを作成しました

---
+++ step2_4.java
@@ -0,0 +1,13 @@
+public class step2_4 {
+    public static void main(String[] args){
+        int i = (7);
+        for(int j = 1; j < 10; j++) {
+        System.out.print(i + "×" + j + "=" + k(7,1));
+        System.out.println("");
+        }     
+    }
+
+    private static int k(int i, int j){
+        return i * j;  
+    }
+}
