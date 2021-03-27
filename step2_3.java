aa89049300ea8ee558271ac8f16568ee767e2036
akie27 <akie27@outlook.jp>
2021-03-27 21:47:00 +0900

    step2_3.javaを修正しました

--- step2_3.java
+++ step2_3.java
@@ -1,9 +1,9 @@
 public class step2_3 {
     public static void main(String args[]){
         name("服部");
-      }
-      
-      private static void name(String str){
-        System.out.println(str + "さんこんにちは！");
-      }
+    }
+    
+    private static void name(String str){
+    System.out.println(str + "さんこんにちは！");
+    }
 }
