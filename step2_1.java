463350e7b90580cb2f796b2a85b023aec1fcac9f
akie27 <akie27@outlook.jp>
2021-03-27 18:07:10 +0900

    step2_1.javaを修正しました

--- step2_1.java
+++ step2_1.java
@@ -1,15 +1,11 @@
-import java.util.Scanner;
 public class step2_1 {
     public static void main(String args[]){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("1つ目の数字を入力してください: ");
-        int num1 = scanner.nextInt();
-        System.out.print("2つ目の数字を入力してください: ");
-        int num2 = scanner.nextInt();
-        int sum = num1 + num2;
-        
-        System.out.println(num1 + "+" + num2 + "=" +  sum);
+        int sum = add(1, 2);
 
-        scanner.close();
+        System.out.println(sum);
+    }
+    
+    public static int add(int num1, int num2) {
+        return num1 + num2;
     }
 }
