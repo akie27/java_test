378f01ebe4fade6d8e4e1b4cad5bcfe56f0d03e8
akie27 <akie27@outlook.jp>
2021-03-28 14:40:22 +0900

    step2_5.javaを修正しました

--- step2_5.java
+++ step2_5.java
@@ -1,14 +1,16 @@
 public class step2_5 {
     public static void main(String[] args) {
 
-        int [] data = {14,38,9,41,19};
-        System.out.println("最小値は"+min(data)+"です。");
+        int array[] = {14,38,9,41,19};
+        System.out.println("最小値は"+min(array)+"です。");
     }
 
-    private static int min(int [] data){ 
-        int min = Integer.MAX_VALUE;
-        for(int i = 1 ; i < data.length; i++){
-            min = Math.min(min, data[i]);                                
+    private static int min(int array[]){ 
+        int min = array[0];
+        for(int i = 0 ; i < array.length; i++){
+            if(min > array[i]) {
+                min = array[i];
+            }                             
         }
         return min; 
     }
