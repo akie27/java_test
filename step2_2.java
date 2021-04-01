9f731c8550ed2d95ccc162b43275f25c8346a49a
akie27 <akie27@outlook.jp>
2021-04-01 18:08:33 +0900

    step2_2.javaで文字列で返すがうまくできません

--- step2_2.java
+++ step2_2.java
@@ -2,19 +2,24 @@ public class step2_2 {
     public static double BMI(double height, double weight) {
         height = height / 100;
         double bmi = weight / (height * height);
-        return bmi;
+
+        String str = "低体重,標準,肥満";
+        String[] str1 = str.str1("低体重");
+        String[] str2 = str.str2("標準");
+        String[] str3 = str.str3("肥満");
+        return str;
     }
     public static void main(String[] args) {
         double bmi = BMI(170, 60); 
         System.out.println(String.format("%.1f",bmi));
         if( bmi < 18.5 ){
-            System.out.println( "低体重" );
+            System.out.println(str1);
         }      
         else if( bmi < 25.0 ){
-            System.out.println( "標準" );
+            System.out.println(str2);
         } 
         else{
-            System.out.println( "肥満" );
+            System.out.println(str3);
         } 
     }
 }
